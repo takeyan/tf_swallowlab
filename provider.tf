f344f346b39917fbf733d9cbdced5568e5340dc5
takeyan <takeyan.g@gmail.com>
2019-07-31 03:49:25 +0000

    Infrastructure is up-to-date

--- provider.tf
+++ provider.tf
@@ -1,30 +1,16 @@
-variable "softlayer_username" {
-  description = "Enter your IBM Infrastructure (SoftLayer) full username, you can get this using: https://control.bluemix.net/account/user/profile"
-}
+variable "softlayer_username" {}
+variable "softlayer_api_key" {}
+variable "ibmcloud_api_key" {}
 
-variable "softlayer_api_key" {
-  description = "Enter your IBM Infrastructure (SoftLayer) API key, you can get this using: https://control.bluemix.net/account/user/profile"
-}
-
-variable "bluemix_api_key" {
-  description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
-}
-
-variable "cloudflare_username" {
-  description = "cloudflare email address"
-}
-
-variable "cloudflare_api_key" {
-  description = "cloudflare api key"
-}
-
+variable "cloudflare_username" {}
+variable "cloudflare_api_key" {}
 variable "cloudflare_domain" {}
 
 
 provider "ibm" {
   softlayer_username = "${var.softlayer_username}"
   softlayer_api_key  = "${var.softlayer_api_key}"
-  bluemix_api_key    = "${var.bluemix_api_key}"
+  ibmcloud_api_key   = "${var.ibmcloud_api_key}"
 }
 
 provider "cloudflare" {
